2d079f95063d4cdc0fa4f98fc6e2dccb3a41595b
Lucas Gabriel Silva Perseguin <110643948+Lucas-Perseguin@users.noreply.github.com>
2023-03-12 20:23:46 -0300

    feat: use redis to create cache of event data

--- src/services/events-service/index.ts
+++ src/services/events-service/index.ts
@@ -3,25 +3,51 @@ import eventRepository from "@/repositories/event-repository";
 import { exclude } from "@/utils/prisma-utils";
 import { Event } from "@prisma/client";
 import dayjs from "dayjs";
+import { createClient } from "redis";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const redis = createClient({
+  url: process.env.REDIS_URL,
+});
+
+redis.connect();
 
 async function getFirstEvent(): Promise<GetFirstEventResult> {
-  const event = await eventRepository.findFirst();
-  if (!event) throw notFoundError();
+  const cachedEvent = await redis.get("event");
+  if (cachedEvent) {
+    return exclude(JSON.parse(cachedEvent) as Event, "createdAt", "updatedAt");
+  } else {
+    const event = await eventRepository.findFirst();
+    if (!event) throw notFoundError();
+    await redis.set("event", JSON.stringify(event));
 
-  return exclude(event, "createdAt", "updatedAt");
+    return exclude(event, "createdAt", "updatedAt");
+  }
 }
 
 export type GetFirstEventResult = Omit<Event, "createdAt" | "updatedAt">;
 
 async function isCurrentEventActive(): Promise<boolean> {
-  const event = await eventRepository.findFirst();
-  if (!event) return false;
+  const cachedEvent = await redis.get("event");
+  if (cachedEvent) {
+    const now = dayjs();
+    const eventStartsAt = dayjs(JSON.parse(cachedEvent).startsAt);
+    const eventEndsAt = dayjs(JSON.parse(cachedEvent).endsAt);
 
-  const now = dayjs();
-  const eventStartsAt = dayjs(event.startsAt);
-  const eventEndsAt = dayjs(event.endsAt);
+    return now.isAfter(eventStartsAt) && now.isBefore(eventEndsAt);
+  } else {
+    const event = await eventRepository.findFirst();
+    if (!event) return false;
+    await redis.set("event", JSON.stringify(event));
 
-  return now.isAfter(eventStartsAt) && now.isBefore(eventEndsAt);
+    const now = dayjs();
+    const eventStartsAt = dayjs(event.startsAt);
+    const eventEndsAt = dayjs(event.endsAt);
+
+    return now.isAfter(eventStartsAt) && now.isBefore(eventEndsAt);
+  }
 }
 
 const eventsService = {
